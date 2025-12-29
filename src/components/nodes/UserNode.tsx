93b81226779c2633b4aafdbd18506d3bbca1aaee
srkmyk <woxqaq@gmail.com>
2025-12-29 15:41:51 +0800

    fix: layout algo (#8)
    * chore: fix layout algo
    
    * adjust const
    
    * docs

--- src/components/nodes/UserNode.tsx
+++ src/components/nodes/UserNode.tsx
@@ -36,7 +36,8 @@ export function UserNode({
 	return (
 		<>
 			{/* Wrapper to extend hover area for buttons outside the node */}
-			<div className="relative pr-8">
+			{/* Note: Negative margin (-mr-8) compensates for padding to keep node dimensions consistent */}
+			<div className="relative pr-8 -mr-8">
 				{/* Branch Button (+) - Only visible when hovered, positioned to the right */}
 				{isHovered && onCreateChild && (
 					<button
